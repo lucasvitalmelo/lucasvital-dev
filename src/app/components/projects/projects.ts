41da3636fe9266b045ba4ada998470ca6f77c122
Lucas Vital <lucasvitaldemelo@gmail.com>
2025-06-23 22:54:30 -0300

    docs: add new porjects

--- src/app/components/projects/projects.ts
+++ src/app/components/projects/projects.ts
@@ -1,28 +1,42 @@
 export const projects = [
   {
-    name: "Letmeask",
-    link: "https://letmeask-nlw-528ce.firebaseapp.com/",
+    name: 'Manager Tattoo',
+    link: 'https://manager-web-production.up.railway.app/',
+    code: 'https://github.com/lucasvitalmelo/manager-web',
+    image: '/projects/manager.png',
+    description: 'Manage your tattoo budgets.',
+    stacks: `ReactJs, NestJs, postgres`
+  },
+  {
+    name: 'Letmeask',
+    link: 'https://letmeask-nlw-528ce.firebaseapp.com/',
     code: 'https://github.com/lucasvitalmelo/next-level-week-projects/tree/main/letmeask',
-    image: "/projects/letmeask-image.png",
-    description: "Create your Q&A rooms quickly!",
+    image: '/projects/letmeask-image.png',
+    description: 'Create your Q&A rooms quickly!',
     stacks: `ReactJs, Sass, Firebase`
   },
   {
-    name: "Todo List",
-    link: "https://ignite-todo-2023.vercel.app/",
+    name: 'Agrotis - case',
+    link: 'https://agrotis.vercel.app/',
+    code: 'https://github.com/lucasvitalmelo/agrotis',
+    image: '/projects/agrotis.png',
+    description: 'Case made for the company Agrotis',
+    stacks: `ReactJs, context-api, material-ui`
+  },
+  {
+    name: 'Todo List',
+    link: 'https://ignite-todo-2023.vercel.app/',
     code: 'https://github.com/lucasvitalmelo/next-level-week-projects/tree/main/to-do-react',
-    image: "/projects/todo-project.png",
-    description: "Classic todo list!",
+    image: '/projects/todo-project.png',
+    description: 'Classic todo list!',
     stacks: `ReactJs, Styled Components, React Hook Form`
   },
   {
-    name: "Coffee Delivery",
-    link: "https://coffee-delivery-two-gamma.vercel.app/",
+    name: 'Coffee Delivery',
+    link: 'https://coffee-delivery-two-gamma.vercel.app/',
     code: 'https://github.com/lucasvitalmelo/02-dasafio-coffee-delivery-reactts-2023',
-    image: "/projects/coffee-delivery-project.png",
-    description: "E-commerce for coffee delivery!",
+    image: '/projects/coffee-delivery-project.png',
+    description: 'E-commerce for coffee delivery!',
     stacks: `ReactJs, Styled Components, Zod, React Hook Form`
   },
-
-
 ]
